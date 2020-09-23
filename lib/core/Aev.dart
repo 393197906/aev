f827ba3aed8b53cecb63d68f15f84e14f713ae9a
mr.xie <393197906@qq.com>
2020-09-23 11:44:59 +0800

    aev exceptionhandler

--- lib/core/Aev.dart
+++ lib/core/Aev.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:aev/Toast/index.dart';
 import 'package:aev/core/AevBaseWidget.dart';
 import 'package:aev/fetch/Fetch.dart';
@@ -8,14 +10,21 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 typedef Widget UseModelHandel(Widget child);
+typedef ExceptionHandler(dynamic error, dynamic stackTrace);
+
+bool get isInDebugMode {
+  bool inDebugMode = false;
+  assert(inDebugMode = true);
+  return inDebugMode;
+}
 
 class Aev {
   static Aev instance;
-
   AevRouter _aevRouter;
   ThemeData _aevTheme;
   String _aevTitle = "flutter应用";
   UseModelHandel _useModelHandel;
+  ExceptionHandler _exceptionHandler;
 
   Aev._();
 
@@ -23,6 +32,11 @@ class Aev {
     if (instance != null) return instance;
     instance = Aev._();
     return instance;
+  }
+
+  Aev useExceptionHandler(ExceptionHandler exceptionHandler) {
+    this._exceptionHandler = exceptionHandler;
+    return this;
   }
 
   Aev useModel(UseModelHandel useModelHandel) {
@@ -54,6 +68,7 @@ class Aev {
     return this;
   }
 
+  // ioc 初始化
   _iocHelper() {
     // 注册Toast
     Ioc.provider<Toast>(Toast());
@@ -64,14 +79,41 @@ class Aev {
 
   Aev start() {
     _iocHelper();
+    // This captures errors reported by the Flutter framework.
+    // flutter 异常
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      if (isInDebugMode) {
+        // In development mode simply print to console.
+        FlutterError.dumpErrorToConsole(details);
+      } else {
+        // In production mode report to the application zone to report to
+        // Sentry.
+        Zone.current.handleUncaughtError(details.exception, details.stack);
+      }
+    };
+
     final aevBaseWidget = AevBaseWidget(
       aevRouter: this._aevRouter,
       theme: this._aevTheme,
       title: this._aevTitle,
     );
-    this._useModelHandel != null
-        ? runApp(this._useModelHandel(aevBaseWidget))
-        : runApp(aevBaseWidget);
+
+    runZonedGuarded<Future<Null>>(() async {
+      this._useModelHandel != null
+          ? runApp(this._useModelHandel(aevBaseWidget))
+          : runApp(aevBaseWidget);
+    }, (error, stackTrace) async {
+      // 捕获异常
+      print('Aev Caught error: $error');
+      if (isInDebugMode) {
+        print(stackTrace);
+        return;
+      }
+      if (this._exceptionHandler != null) {
+        await this._exceptionHandler(error, stackTrace);
+      }
+    });
+
     return this;
   }
 }
